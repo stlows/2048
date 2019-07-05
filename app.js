eaa785bbfe2fb3b1986661f4b9b9e6f27bea0ae9
vbeaulieu <vbeaulieu@soquij.qc.ca>
2019-07-05 11:49:17 -0400

    change grid size (style custom dans le js)

--- app.js
+++ app.js
@@ -3,15 +3,88 @@ let fontSizesClass = ["bigger", "big", "normal", "small", "smaller"];
 let score = 0;
 let odds2 = 0.9;
 let defaultBorderColor = "black";
-let gridSize = 4;
+let gridSize = 3;
 let goal = 2048;
 let alreadyWon = false;
+let tileSize = 100;
+let tileSizeSmallFactor = 0.5;
+let smallScreen = 700;
 
-function createEmptyTiles(n) {
+function generateSpecialCss() {
+  let styleSheet = document.createElement("style");
+  let customCss = document.getElementById("custom-css");
+  if (customCss) {
+    customCss.parentElement.removeChild(customCss);
+  }
+  let gridGap = Math.floor(20 / gridSize);
+  let gameBoardWidth = tileSize * gridSize + gridGap * (gridSize + 1);
+  let gameBoardWidthSmall = tileSize * gridSize * tileSizeSmallFactor + gridGap * (gridSize + 1);
+  styleSheet.id = "custom-css";
+  styleSheet.innerHTML = `
+    #game-board {
+      width: ${gameBoardWidth}px;
+      grid-template-columns: repeat(${gridSize}, 1fr);
+      grid-gap: ${gridGap}px;
+      border: ${gridGap}px solid #333;
+    }
+    #game-board .tile {
+      width: ${tileSize}px;
+      height: ${tileSize}px;
+    }
+    #game-board .tile .number {
+      line-height: ${tileSize}px;
+    }
+    #game-board .tile .number.bigger{
+      font-size: ${0.8 * tileSize}px;
+    }
+    #game-board .tile .number.big{
+      font-size: ${0.8 * tileSize}px;
+    }
+    #game-board .tile .number.normal{
+      font-size: ${0.6 * tileSize}px;
+    }
+    #game-board .tile .number.small{
+      font-size: ${0.4 * tileSize}px;
+    }
+    #game-board .tile .number.smaller{
+      font-size: ${0.35 * tileSize}px;
+    }
+    @media screen and (max-width: ${smallScreen}px){
+      #game-board{
+        width: ${gameBoardWidthSmall}px;
+      }
+      #game-board .tile{
+        width: ${tileSize * tileSizeSmallFactor}px;
+        height: ${tileSize * tileSizeSmallFactor}px;
+      }
+      #game-board .tile .number{
+        line-height: ${tileSize * tileSizeSmallFactor}px;
+      }
+      #game-board .tile .number.bigger{
+        font-size: ${0.8 * tileSize * tileSizeSmallFactor}px;
+      }
+      #game-board .tile .number.big{
+        font-size: ${0.8 * tileSize * tileSizeSmallFactor}px;
+      }
+      #game-board .tile .number.normal{
+        font-size: ${0.6 * tileSize * tileSizeSmallFactor}px;
+      }
+      #game-board .tile .number.small{
+        font-size: ${0.4 * tileSize * tileSizeSmallFactor}px;
+      }
+      #game-board .tile .number.smaller{
+        font-size: ${0.35 * tileSize * tileSizeSmallFactor}px;
+      }
+    }`;
+
+  document.body.appendChild(styleSheet);
+}
+
+function createEmptyTiles() {
   let tiles = [];
-  for (let i = 0; i < Math.pow(n, 2); i++) {
-    let x = i % 4;
-    let y = Math.floor(i / 4);
+  for (let i = 0; i < Math.pow(gridSize, 2); i++) {
+    let x = i % gridSize;
+    let y = Math.floor(i / gridSize);
     tiles.push({
       x: x,
       y: y,
@@ -28,7 +101,7 @@ function getEmptyTiles() {
 function flashBoardBorder(color, times, interval) {
   let board = document.getElementById("game-board");
   let count = 1;
-  let flashId = setInterval(function () {
+  let flashId = setInterval(function() {
     if (board.style.borderColor === defaultBorderColor) {
       board.style.borderColor = color;
     } else {
@@ -72,7 +145,6 @@ function getTileDOMObject(tile) {
   return tileDiv;
 }
 
-
 function addNewTile() {
   let emptyTiles = getEmptyTiles();
   let r = randomElement(emptyTiles);
@@ -102,40 +174,39 @@ function noPossibleMove() {
     }
   }
   return true;
-
 }
 
 function getNeighbors(tile) {
   let result = [];
   if (tile.y > 0) {
-    result.push(getTile(tile.x, tile.y - 1))
+    result.push(getTile(tile.x, tile.y - 1));
   }
   if (tile.x > 0) {
-    result.push(getTile(tile.x - 1, tile.y))
+    result.push(getTile(tile.x - 1, tile.y));
   }
   if (tile.y < Math.sqrt(tiles.length) - 1) {
-    result.push(getTile(tile.x, tile.y + 1))
+    result.push(getTile(tile.x, tile.y + 1));
   }
   if (tile.x < Math.sqrt(tiles.length) - 1) {
-    result.push(getTile(tile.x + 1, tile.y))
+    result.push(getTile(tile.x + 1, tile.y));
   }
   return result;
 }
 
 function getTile(x, y) {
-  return tiles.find(t => t.x === x && t.y === y)
+  return tiles.find(t => t.x === x && t.y === y);
 }
 
 function gameOver() {
-  popup("You lost...<br>You scored " + score + " points.")
+  popup("You lost...<br>You scored " + score + " points.");
 }
 
 function popup(msg) {
   let board = document.getElementById("game-board");
   let currentPopup = document.getElementById("popup");
   if (currentPopup) {
-    currentPopup.parentElement.removeChild(currentPopup)
-  };
+    currentPopup.parentElement.removeChild(currentPopup);
+  }
   let overlap = document.createElement("div");
   overlap.classList.add("overlap");
   overlap.id = "popup";
@@ -146,8 +217,11 @@ function popup(msg) {
   board.appendChild(overlap);
 }
 
-
 function move(e) {
+  if (noPossibleMove()) {
+    newGame();
+    return;
+  }
   let validMove = ["ArrowRight", "ArrowLeft", "ArrowDown", "ArrowUp"].some(s => s === e.key);
   let somethingMoved = false;
   if (validMove) {
@@ -267,36 +341,42 @@ function randomElement(a) {
 }
 
 function newGame() {
+  generateSpecialCss();
   tiles = createEmptyTiles(gridSize);
+  score = 0;
   addNewTile();
   addNewTile();
   updateBoard();
 }
+function btnChangeSize() {
+  popup("<p>Enter size (2 and 8):</p><input type='number' id='newGridSize' value='4' /><br><button onclick='changeSize()' class='btn'>Go</button>");
+}
+function changeSize() {
+  let newGridSize = document.getElementById("newGridSize").value;
+  gridSize = parseInt(newGridSize);
+  newGame();
+}
 
 function saveGame() {
-  let encodedPowers = tiles.map(t => t.value ? (Math.log(t.value) / Math.log(2)).toString(16) : 0).join("");
-  popup("<p>Copy this code and load a game using it later:</p><p>" + encodedPowers + "</p><p>Score will be lost.</p>")
+  let encodedPowers = tiles.map(t => (t.value ? (Math.log(t.value) / Math.log(2)).toString(16) : 0)).join("");
+  popup("<p>Copy this code and load a game using it later:</p><p>" + encodedPowers + "</p><p>Score will be lost.</p>");
 }
 
 function btnLoadGame() {
-  popup("<p>Enter code:</p><input type='text' id='encodedPowers' /><br><button onclick='loadGame()' class='btn'>Go</button>")
+  popup("<p>Enter code:</p><input type='text' id='encodedPowers' /><br><button onclick='loadGame()' class='btn'>Go</button>");
 }
 
 function loadGame() {
   let encodedPowers = document.getElementById("encodedPowers").value;
-  console.log(encodedPowers);
-  console.log(encodedPowers.length);
-  if (encodedPowers.length !== 16) {
-    popup("Incorect code.<br>Try again...")
+  if ([4, 9, 16, 25, 36, 49, 64].every(i => i !== encodedPowers.length)) {
+    popup("Incorect code.<br>Try again...");
     return;
   }
-  let newTiles = encodedPowers
-    .split("")
-    .map(s => s === "0" ? null : Math.pow(2, parseInt(s, 16)));
+  let newTiles = encodedPowers.split("").map(s => (s === "0" ? null : Math.pow(2, parseInt(s, 16))));
   tiles = createEmptyTiles(gridSize);
   score = 0;
   for (let i = 0; i < tiles.length; i++) {
-    tiles[i].value = newTiles[i]
+    tiles[i].value = newTiles[i];
   }
   updateBoard();
 }
